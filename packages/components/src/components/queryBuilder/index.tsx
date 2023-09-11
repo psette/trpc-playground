5099bd6489c240d222a9f5e0a0cc0b017e8141d9
Pietro Sette <settepietro33@gmail.com>
2023-09-10 20:40:12 -0700

    Group and sort

--- packages/components/src/components/queryBuilder/index.tsx
+++ packages/components/src/components/queryBuilder/index.tsx
@@ -1,22 +1,22 @@
-import { ChevronUpIcon } from '@heroicons/react/24/solid'
+import { ChevronUpIcon, SearchIcon } from '@heroicons/react/solid'
 import { atom, useAtom, WritableAtom } from 'jotai'
-import { useCallback } from 'preact/hooks'
+import { useCallback, useState } from 'preact/hooks'
 import { Resizable } from 're-resizable'
+import React from 'react'
 import { typesAtom } from '../provider'
 import { editorAtom, queryBuilderOpened } from '../tab/store'
 import { getDefaultOperation } from './getDefaultOperation'
 
 const generatedAtom = atom<string | null>(null)
 const baseOperationNameAtom = atom<string | null>(null)
+
 // Updates `baseOperationNameAtom` and `generatedAtom`
 const operationNameAtom: WritableAtom<string | null, string> = atom(
   get => get(baseOperationNameAtom),
   (get, set, update) => {
     set(baseOperationNameAtom, update)
-
     const types = get(typesAtom)
     if (!types) return
-
     const defaultOp = getDefaultOperation({ types, operationName: update })
     if (defaultOp?.value) {
       set(generatedAtom, defaultOp.value)
@@ -24,47 +24,48 @@ const operationNameAtom: WritableAtom<string | null, string> = atom(
   },
 )
 
-export const QueryBuilder = () => {
+export const QueryBuilder: React.FC = () => {
   const [types] = useAtom(typesAtom)
   const [queryBuilderOpen, setQueryBuilderOpened] = useAtom(queryBuilderOpened)
   const [generated] = useAtom(generatedAtom)
   const [operationName, setOperationName] = useAtom(operationNameAtom)
   const [editorView] = useAtom(editorAtom)
 
+  const [searchQuery, setSearchQuery] = useState<string>('')
+
   // On insert btn click
-  const insertGenerated = useCallback(
-    () => {
-      if (!operationName || !editorView) return
-      const gen = getDefaultOperation({ operationName, types })
-      if (!gen) return
-
-      const { value: generated, inputLength } = gen
-      const cursor = editorView.state.selection.asSingle().ranges[0]
-
-      editorView.focus()
-      // Add generated query to current line in editor
+  const insertGenerated = useCallback(() => {
+    if (!operationName || !editorView) return
+    const gen = getDefaultOperation({ operationName, types })
+    if (!gen) return
+    const { value: generated, inputLength } = gen
+    const cursor = editorView.state.selection.asSingle().ranges[0]
+    editorView.focus()
+    // Add generated query to the current line in the editor
+    editorView.dispatch({
+      changes: {
+        from: cursor.from,
+        to: cursor.to,
+        insert: generated,
+      },
+    })
+    // Select generated default input so the user can instantly remove/edit
+    if (cursor.from + generated.length - inputLength !== cursor.from + generated.length) {
       editorView.dispatch({
-        changes: {
-          from: cursor.from,
-          to: cursor.to,
-          insert: generated,
+        selection: {
+          anchor: cursor.from + generated.length - inputLength - 1,
+          head: cursor.from + generated.length - 1,
         },
       })
-
-      // Select generated default input so user can instantly remove/edit
-      if (cursor.from + generated.length - inputLength !== cursor.from + generated.length) {
-        editorView.dispatch({
-          selection: {
-            anchor: cursor.from + generated.length - inputLength - 1,
-            head: cursor.from + generated.length - 1,
-          },
-        })
-      }
-    },
-    [editorView, operationName, types],
-  )
+    }
+  }, [editorView, operationName])
 
   if (!types) return null
+
+  // Filter the operation names based on the search query
+  const filteredOperationNames = Object.keys({ ...types.queries, ...types.mutations }).filter(
+    opName => opName.includes(searchQuery)
+  )
 
   return (
     <Resizable
@@ -79,11 +80,13 @@ export const QueryBuilder = () => {
           className='flex justify-between mx-3 pb-1 items-center h-6 cursor-pointer'
           onClick={() => setQueryBuilderOpened(open => !open)}
         >
-          <p className={'text-neutral-100	mx-1 font-semibold'}>Query builder</p>
+          <p className={'text-neutral-100 mx-1 font-semibold'}>Query builder</p>
           <button>
-            {queryBuilderOpen
-              ? <ChevronUpIcon width={18} height={18} className='rotate-180' />
-              : <ChevronUpIcon width={18} height={18} className='rotate-0' />}
+            {queryBuilderOpen ? (
+              <ChevronUpIcon width={18} height={18} className='rotate-180' />
+            ) : (
+              <ChevronUpIcon width={18} height={18} className='rotate-0' />
+            )}
           </button>
         </div>
         {queryBuilderOpen && (
@@ -91,16 +94,32 @@ export const QueryBuilder = () => {
             <span className='font-semibold'>Operations</span>
             <div className='pt-3'>
               <div className='ml-1'>
-                {Object.keys({ ...types.queries, ...types.mutations }).sort().map((opName) => (
-                  <button
-                    key={opName}
-                    className='bg-secondary mx-1 px-2 shadow-lg mb-1'
-                    style={{ color: operationName === opName ? 'white' : 'gray' }}
-                    onClick={() => setOperationName(opName)}
-                  >
-                    {opName}
-                  </button>
-                ))}
+                <div className='relative'>
+                  <input
+                    type='text'
+                    placeholder='Search operations...'
+                    className='w-full py-1 px-2 bg-secondary rounded-md text-neutral-100 focus:outline-none focus:ring focus:ring-primary'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                  <SearchIcon className='absolute top-2 left-2 h-5 w-5 text-neutral-300' />
+                </div>
+                {filteredOperationNames.length === 0 ? (
+                  <p className='text-neutral-300'>No matching operations found.</p>
+                ) : (
+                  <div className='pt-2'>
+                    {filteredOperationNames.map((opName) => (
+                      <button
+                        key={opName}
+                        className='bg-secondary mx-1 px-2 shadow-lg mb-1'
+                        style={{ color: operationName === opName ? 'white' : 'gray' }}
+                        onClick={() => setOperationName(opName)}
+                      >
+                        {opName}
+                      </button>
+                    ))}
+                  </div>
+                )}
               </div>
               <div className='pt-4'>
                 <button
